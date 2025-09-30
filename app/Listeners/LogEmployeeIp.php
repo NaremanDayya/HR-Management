da11747cfd4c4167e19ac3cdedb77a54851e81eb
NaremanDayya <nanadaya166@gmail.com>
2025-09-30 13:25:36 +0300

    impersonate issue solving

--- app/Listeners/LogEmployeeIp.php
+++ app/Listeners/LogEmployeeIp.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Request;
 use App\Models\User;
 use App\Notifications\BlockedIpLoginAttempt;
+use Illuminate\Support\Facades\Session;
 
 class LogEmployeeIp
 {
